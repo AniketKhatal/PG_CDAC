b6d55ca873d0d08d8513709e4ca09c5921133ecc
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-05-05 11:34:02 +0530

    CPP first commit

---
+++ cpp/Aniket Khatal 240101600/AreaOfRect.cpp
@@ -0,0 +1,18 @@
+
+//WAP to calulate the area of the Rectangle
+//Area=l * b;
+
+#include<iostream>
+using namespace std;
+int main(){
+	double l;
+	double b;
+	double area;
+	cout<<"Enter the Lenght and breadth of the rectangle:";
+	cin>>l;
+	cin>>b;
+	
+	area=l*b;
+	cout<<"The area of the Ractangle is:"<<area<<endl;
+	return 0;
+}
