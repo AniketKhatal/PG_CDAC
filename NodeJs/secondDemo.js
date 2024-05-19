04cced8427d449af8ce7d43a59da3a4bd8fbc82f
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-05-19 13:05:09 +0530

    code commit

---
+++ NodeJs/secondDemo.js
@@ -0,0 +1,19 @@
+//http
+const http= require("http");
+
+const server=http.createServer((req,resp) =>{
+    //req: request process
+    //resp:for writting response
+
+    resp.writeHead(200,{"content-type":"text/html"});
+    resp.write("<h1>Wow this is response from first created server</h1>");
+    resp.write("<h2>Okk thats nice</h2>");
+    resp.end("Ok Ok ");
+});
+server.listen(3001);
+
+
+// how to run the node js program
+// save file with .js fromate in the program last mention port num like server.listen(3001);
+// run code in vs code and then go to browser
+//open the broweser and type localhost:portnum like localhost:3001 and hit enter
