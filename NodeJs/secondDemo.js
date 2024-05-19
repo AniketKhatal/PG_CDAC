d9dc7978be9c3e881ce760f12f85818f621e4679
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-05-19 13:07:17 +0530

    commit

--- NodeJs/secondDemo.js
+++ NodeJs/secondDemo.js
@@ -16,4 +16,4 @@ server.listen(3001);
 // how to run the node js program
 // save file with .js fromate in the program last mention port num like server.listen(3001);
 // run code in vs code and then go to browser
-//open the broweser and type localhost:portnum like localhost:3001 and hit enter
+//open the broweser and type localhost:portnum like localhost:3001  you can also do like http://localhost:3001/secondDemo.js and hit enter
