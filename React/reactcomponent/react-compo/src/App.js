19627699d9d116ab3ba97113ce7b0faedff08bab
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-05-24 01:56:31 +0530

    react commit

---
+++ React/reactcomponent/react-compo/src/App.js
@@ -0,0 +1,15 @@
+import akbutton from "./akbutton";
+
+function app(){
+  return <div>
+    <h1>
+    This is mt first react component<br></br>
+    hi pappi shet nad rockey Shet<hr></hr> 
+    Ksl yyy aaaa Khasch !!!
+  </h1>
+  <button>Hit me</button>
+  <akbutton></akbutton>
+  </div>
+}
+
+export default app;
