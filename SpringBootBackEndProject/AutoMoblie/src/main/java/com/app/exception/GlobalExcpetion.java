02af64df908fc92af10c5efd41237533d0e8446c
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-19 01:59:08 +0530

    boot commit

--- SpringBootBackEndProject/AutoMoblie/src/main/java/com/app/exception/GlobalExcpetion.java
+++ SpringBootBackEndProject/AutoMoblie/src/main/java/com/app/exception/GlobalExcpetion.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExcpetion {
 
-	@ExceptionHandler(ResourceNotFound.class)
-	public ResponseEntity<?> handleResourceNotFound(ResourceNotFound e) {
-
-		return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
-	}
-
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    
+    // Other exception handlers...
 }
