8b8276837ce634ed250c274c7052b04c9d6e2dd1
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-21 23:01:13 +0530

    Commit

---
+++ DSA/Array/FindArrayPairs.java
@@ -0,0 +1,13 @@
+//Printing the Array Pairs and also printing the pairs count of the array;
+
+import java.util.*;
+public class FindArrayPairs {
+public static void printPairs(int Arr[]){
+    for(int i=0;i<Arr.length;i++){
+        int curr = Arr[i]
+        for(int j=i+1;j<Arr.length;j++){
+            
+        }
+    }
+}  
+}
