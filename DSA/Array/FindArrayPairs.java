788527f434585054d22925d53846047eedc52cb6
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-21 23:14:43 +0530

    Commit

--- DSA/Array/FindArrayPairs.java
+++ DSA/Array/FindArrayPairs.java
@@ -1,13 +1,23 @@
 //Printing the Array Pairs and also printing the pairs count of the array;
+// /To fin dout the tota number if the pairs we have formula  i.e. n(n-1)/2
 
-import java.util.*;
 public class FindArrayPairs {
-public static void printPairs(int Arr[]){
-    for(int i=0;i<Arr.length;i++){
-        int curr = Arr[i]
-        for(int j=i+1;j<Arr.length;j++){
-            
+
+    public static void printPairs(int Arr[]) {
+        int count=0; 
+        for (int i = 0; i < Arr.length; i++) {
+            int curr = Arr[i];  //  1,4,2,3,5,8
+            for (int j = i + 1; j < Arr.length; j++) {
+                System.out.printf("(" + curr + "," + Arr[j] + ")");
+                count++;
+            }
+            System.out.println();
         }
+        System.out.println("The total number of the pairs is:"+count);
     }
-}  
+
+    public static void main(String[] args) {
+        int Arr[] = {1, 4, 2, 3, 5, 8};
+        printPairs(Arr);
+    }
 }
