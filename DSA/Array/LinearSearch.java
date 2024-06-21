47e7ecbe57dfc59d8d71aedeb62b195625dad483
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-21 17:11:24 +0530

    DSA Commit

---
+++ DSA/Array/LinearSearch.java
@@ -0,0 +1,23 @@
+//Linear Search in java
+
+import java.util.*;
+public class LinearSearch{
+    public static void main(String x[]){
+        try(Scanner sc=new Scanner(System.in)) {
+        System.out.println("Enter the size of the Array:");
+        int Arr[]=new int[sc.nextInt()];
+        System.out.println("Enter the Element into the Array:");
+        for(int i=0;i<Arr.length;i++){
+            Arr[i]=sc.nextInt();
+        }  
+        System.err.println("Enter the element that you want to search:");
+        int Ele=sc.nextInt();
+        int count=0,index=0;
+        //Search the element in the Array in the linear manner
+        for(int i=0;i<Arr.length;i++){
+            if(Arr[i]==Ele){
+                count=1;
+                index=i+1;
+                break;
+        }
+    }
