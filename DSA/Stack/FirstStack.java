fb13e484ced4736f59d734b2683a78674af1940a
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-24 00:38:42 +0530

    Stack Commit

---
+++ DSA/Stack/FirstStack.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class FirstStack {
+   static class Stack{
+    //creation of the Stack
+    static ArrayList<Integer> list =new ArrayList<>();
+    public static boolean isEmpty() {
+        return list.size() == 0;
+    }
+    //push operation
+    public static void push(int data){
+        list.add(data);
+    }
+    //pop
+    public static int pop(){
+        int top=list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
+    //peek
+    public static int peek(){
+        return list.get(list.size()-1);
+    }
+   } 
+   public static void main(String[] args) {
+       Stack s=new Stack();
+       s.push(1);
+       s.push(2);
+       s.push(3);
+
+       while(!s.isEmpty()){
+        System.err.println(s.peek());
+        s.pop();
+       }
+   }
+}
