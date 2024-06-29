f78c8c605745af0e279153c2583d0714dd511e36
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-06-29 12:18:05 +0530

    Commit

---
+++ DSA/Queue/QueueExUsingLinkedList.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class QueueExUsingLinkedList {
+    public static void main(String[] args) {
+            Queue<Integer> q = new LinkedList<>();
+    
+            // Enqueue elements
+            q.add(50);
+            q.add(40);
+            q.add(15);
+            
+            // Peek at the front element
+            System.out.println("Front element is: " + q.peek());
+    
+            // Dequeue elements
+            System.out.println("Removed element: " + q.remove());
+            System.out.println("Front element is: " + q.peek());
+    
+            // Check if queue is empty
+            System.out.println("Queue is empty: " + q.isEmpty());
+    }
+}
