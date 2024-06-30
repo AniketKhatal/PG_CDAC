6fdcf5c600ca37c4c97120231ded83eb471120c4
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-01 00:14:18 +0530

    commit

--- JAVAByMadhuraM/Practice/src/StringPractice/StringFirst.java
+++
@@ -1,9 +0,0 @@
-package StringPractice;
-public class StringFirst {
-    public static void main(String x[]){
-        String str="King";
-        for(int i=0;i<str.length();i++){
-            System.out.printf(str.charAt(i)+" ");
-        }
-    }    
-}
