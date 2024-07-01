bfbd8ace87e5b1b6e978323b8f3b81f67ab591cb
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-01 16:25:42 +0530

    Commit

--- JAVAByMadhuraM/Practice/src/StringPractice/LetterCount.java
+++ JAVAByMadhuraM/Practice/src/StringPractice/LetterCount.java
@@ -4,23 +4,23 @@
 import java.util.*;
 public class LetterCount{
     public static String compress(String str){
-    String newStr="";
     StringBuilder sb=new StringBuilder();
 
     for(int i=0;i<str.length();i++){
         //we use Integer for count bcz after  counting the char we 
         //want to add into the string in output like a3b2c4 like that
         Integer count=1;    
-        while(i<str.length()-1 && str.charAt(i) ==str.charAt(+1)){
+        //This is the main logic of the program
+        while(i<str.length()-1 && str.charAt(i) ==str.charAt(i+1)){
             count++;
             i++;
         }
-        sb.append(str.charAt(i));
+        sb.append(str.charAt(i));  //add normal if there is only one char is there like abc
         if(count >1){
             sb.append(count.toString());
         }
     }
-    return newStr;
+    return sb.toString();
 }
     public static void main(String[] args) {
         String str="aaabbcdderrr";
