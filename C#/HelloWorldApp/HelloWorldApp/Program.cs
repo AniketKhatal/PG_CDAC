e93d602007becffec1f41051dbff0ddf25f40be9
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-07 01:01:40 +0530

    C# 1st Commit

---
+++ C#/HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Aniket G. Khatal-Patil,WellCome to C# world!");
+        }
+    }
+}
