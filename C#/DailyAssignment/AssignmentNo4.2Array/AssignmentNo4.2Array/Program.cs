036d845758d93443c4268f118580e3981caa9bea
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-09 12:45:24 +0530

    C# Commit

---
+++ C#/DailyAssignment/AssignmentNo4.2Array/AssignmentNo4.2Array/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ 2. Create an array of Employee class objects
+        Accept details for all Employees
+        Display the Employee with highest Salary
+        Accept EmpNo to be searched. Display all details for that employee.
+ */
+namespace AssignmentNo4._2Array
+{
+    public class Employee
+    {
+        //Set the Properties
+        public int EmpNo {  get; set; }
+        public string Name {  get; set; }
+        public double Salary {  get; set; }
+
+        public
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
