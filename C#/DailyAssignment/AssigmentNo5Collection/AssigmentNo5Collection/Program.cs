94528a855cd66875646a5c1c078e971312f96b74
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-09 23:55:43 +0530

    C# Commit

---
+++ C#/DailyAssignment/AssigmentNo5Collection/AssigmentNo5Collection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssigmentNo5Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
