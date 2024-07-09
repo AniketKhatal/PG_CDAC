86c6823d4ee101bd9c927e94a14ac41ae96a9702
Aniket Khatal <aniketgkhatal3742@gmail.com>
2024-07-09 06:35:02 +0530

    Commit

--- C#/DailyAssignment/AssignmentNo3/AssignmentNo3/Program.cs
+++ C#/DailyAssignment/AssignmentNo3/AssignmentNo3/Program.cs
@@ -1,7 +1,75 @@
-﻿namespace AssignmentNo3
+﻿//Inheritence
+
+
+namespace AssignmentNo3
 {
-    internal class Program
+    internal class EmployeeMgnt
     {
+       abstract class Employee
+        {
+            private string name;
+            private string Name
+            {
+                set
+                {
+                    if (value != null)
+                    {
+                        this.name = value;
+                    }
+                    else throw new Exception("No blanks")
+                }
+            }
+            //for autogeneration
+            private static int Empno = 0;
+
+            private short deptno;
+            private short DeptNo
+            {
+                set
+                {
+                    if (value > 0)
+                    {
+                        this.deptno = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Department number must b  greater than Zero");
+                    }
+                }
+            }
+            public Employee(string name,short deptNo=0)
+            {
+                this.Name = name;
+                this.DeptNo = deptNo;
+                Empno++;
+            }
+            public abstract decimal Basic { set; get; }
+            public abstract decimal CalcNetSalary();
+        }
+
+        public class Manager : Employee
+        {
+            private string designation;
+            private string Designation
+            {
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        this.designation = value;
+                    }
+                    else
+                    {
+                        throw new Exception("Cant be blank");
+                    }
+                }
+            }
+            public Manager(string name,short deptNo,string designation)
+            {
+                Designation = designation;
+            }
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
